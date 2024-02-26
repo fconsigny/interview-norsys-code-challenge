1f5b86bc24e50e77690e6763d0f0c0b1cc76f312
Francois Consigny <fconsigny@gmail.com>
2024-02-26 14:37:13 +0100

    [INIT] - init resources with default archetype

---
+++ norsys-training/src/main/java/com/fconsigny/training/norsys/App.java
@@ -0,0 +1,13 @@
+package com.fconsigny.training.norsys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
